f78c34604aa16817c6e5c46f9f7243e19e99e0cc
este0207 <esteban.h0207@gmail.com>
2025-02-04 12:07:33 +0100

    ajout button + case

--- public/script.js
+++ public/script.js
@@ -6,9 +6,15 @@ const secDiv = document.getElementById('sec');
 const image = document.querySelector('.image');
 const button = document.querySelector('.container #add1');
 const button2 = document.querySelector('.container #add2');
+const casebox = document.querySelector('.case');
 // ----------------------------------------\\
 
-
+// -------Objets et Class-------------\\
+let allButtons = [
+    button,
+    button2,
+];
+// ----------------------------------------\\
 // ------Constantes Strings---------\\
 const secTitle = "🍪 - par seconde : ";
 const cookieTitleCount = " Cookies - Cookie-Cliker";
@@ -20,6 +26,8 @@ const cookieNone = "Cookies : none";
 let count = 0;
 let sec = 0;
 let interval;
+let cps = 0; // Cookie Par Secondes
+
 // ------------------------------------------\\
 
 
@@ -29,40 +37,54 @@ function updateAddCookie(nombre){
     countDiv.innerText = count;
     document.getElementById('title').innerText = count + cookieTitleCount;
 }
+function updateRmCookie(nombre){
+    count -= nombre;
+    countDiv.innerText = count;
+    document.getElementById('title').innerText = count + cookieTitleCount;
+}
 
 
 // 🚦- Début du jeu et checkup nécessaires
 if(count === 0){
     document.getElementById('title').innerText = cookieNone
+    
 }
 
-// Attendre que count > a 10 pour afficher button
-if (count < 10) {
-    button.style.background = 'gray';
-    button2.style.background = 'gray';
-}
-
+// Switch pour les prix des améliorations
+switch(count){
+    case count<10:
+        button.style.background = 'gray';
+        button2.style.background = 'gray';
+    case count === 10:
+        button.style.background = 'white';
+    case count === 100:
+        button2.style.background = 'white';
+};
 
 // 🧪 -  Main Et Fonctions principales du jeu :
-
 
 // 🍪 - Bouton Main Cookie +1;
 image.addEventListener('click', () => {
     image.classList.toggle('active');
     updateAddCookie(1);
-    if (count >= 15) {
-        button.style.background = 'white';
-        button2.style.background = 'white';
-    }
+   
 });
 
 // 💞 - Bouton Bonus + Millisec 0.1
+
 button.addEventListener('click', () => {
-    sec = 0.1 ;
-    secDiv.innerText = secTitle + sec;
-    setInterval(() => {
-        updateAddCookie(1);
-    }, 10000);
+    if(count>=10){
+        updateRmCookie(10)
+        casebox.style.display = 'block'
+        sec = 0.1 ;
+        secDiv.innerText = secTitle + sec;
+        interval = setInterval(() => {
+            updateAddCookie(1);
+        }, 10000);
+    }
+    else if(count<10){
+        window.alert("Note Enough Cookies !");
+    }
 
 });
 
@@ -75,5 +97,3 @@ button2.addEventListener('click', () => {
     }, 10000);
     
 });
-
-
