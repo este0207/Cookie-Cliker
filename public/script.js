ae479c4b9d3c807f01a8e7fa9f864b96a09872d0
este0207 <esteban.h0207@gmail.com>
2025-02-04 17:00:18 +0100

    add stone world

--- public/script.js
+++ public/script.js
@@ -1,19 +1,38 @@
 //  🗒️ - Constantes du Code : 
 
 // ------Constantes HTML--------\\
+const body = document.getElementById('.bodyy')
 const countDiv = document.getElementById('count');
 const secDiv = document.getElementById('sec');
 const image = document.querySelector('.image');
+const main = document.querySelector('.main .cookie');
+const title = document.querySelector('.add .case-title')
+const container = document.querySelector('.main .container');
 const button = document.querySelector('.container #add1');
 const button2 = document.querySelector('.container #add2');
+const button3 = document.querySelector('.container #add3');
+const button4 = document.querySelector('.container #add4');
+const buttonDev = document.querySelector('.container #stone-world');
 // ----------------------------------------\\
 
 // -------Objets et Class-------------\\
-let allButtons = [
-    button,
-    button2,
-];
+
+// Button Price
+let buttonPrice = 10; // shovel 0.1
+let button2Price = 100; // axe 1
+let button3Price = 500; // Pickaxe 5
+let button4Price = 1000; // sword  10
+
+let forest1 = false;
+let forest2 = false;
+let forest3 = false;
+let forest4 = false;
+
+
+
+
 // ----------------------------------------\\
+
 // ------Constantes Strings---------\\
 const secTitle = "🍪/s";
 const cookieTitleCount = " Cookies - Cookie-Cliker";
@@ -72,39 +91,78 @@ image.addEventListener('click', () => {
 // 💞 - Bouton Bonus + Millisec 0.1
 
 button.addEventListener('click', () => {
-    if(count>=10){
-        updateRmCookie(10)
-        sec += 0.1;
+    if(count>=buttonPrice){
+        forest1 = true;
+        updateRmCookie(buttonPrice)
+        sec += 1.0;
         secDiv.innerText = sec +secTitle;
-        
         interval = setInterval(() => {
             image.click();
             updateAddCookie(0);
-        }, 10000);
+        }, 1000);
     }
-    else if(count<10){
+    else if(count<buttonPrice){
         window.alert("Pas assez de Cookies !");
     }
 });
 
 // 💞 - Bouton Bonus + Millisec 0.2
 button2.addEventListener('click', () => {
-    if(count>=100){
-        updateRmCookie(100)
-        sec += 1.0;
+    if(count>=button2Price){
+        forest2 = true;
+        updateRmCookie(button2Price)
+        sec += 2.0;
         secDiv.innerText = sec +secTitle;
         interval = setInterval(() => {
             image.click();
             updateAddCookie(1);
         }, 1000);
     }
-    else if(count<100){
+    else if(count<button2Price){
         window.alert("Pas assez de Cookies !");
     }
 });
 
-if(sec === 0.30000000000000004){
-    sec-=0.00000000000000004;
-}
+button3.addEventListener('click', () => {
+    if(count>=button3Price){
+        forest3 = true;
+        updateRmCookie(button3Price)
+        sec += 5.0;
+        secDiv.innerText = sec +secTitle;
+        interval = setInterval(() => {
+            image.click();
+            updateAddCookie(4);
+        }, 1000);
+    }
+    else if(count<button3Price){
+        window.alert("Pas assez de Cookies !");
+    }
+});
 
+button4.addEventListener('click', () => {
+    if(count>=button4Price){
+        forest4 = true;
+        updateRmCookie(button4Price)
+        sec += 15.0;
+        secDiv.innerText = sec +secTitle;
+        interval = setInterval(() => {
+            image.click();
+            updateAddCookie(14);
+        }, 1000);
+    }
+    else if(count<button4Price){
+        window.alert("Pas assez de Cookies !");
+    }
+});
 
+// Stone World
+
+buttonDev.addEventListener('click', () => {
+        document.body.style.backgroundColor = '#808080';
+        title.style.background = 'url(./images/stonetxt.jpg)';
+        main.style.background = 'url(./images/cave.webp)';
+        main.style.backgroundRepeat = 'no-repeat'
+        main.style.backgroundPosition = 'center',
+        main.style.backgroundSize = 'cover',
+        container.style.background = 'url(./images/stonetxt.jpg)';
+});
